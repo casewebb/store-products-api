4245d42d6c3c42c5e5c80fde0af6e32486196f01
Case <case.webb@gmail.com>
2020-05-24 16:56:55 -0400

    Working product POST

--- app/db_models/models.py
+++ app/db_models/models.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 from app import db
 
@@ -9,27 +10,32 @@ product_category = db.Table('product_category', db.Column('category_id', db.Inte
 
 @dataclass
 class Product(db.Model):
-    _id: int
-    title: str
-    description: str
-    image_url: str
-    link: str
-    categories: str
+    product_id: int
+    product_title: str
+    product_description: str
+    product_image_url: str
+    product_link: str
+    product_price: float
+    product_gender: int
+    product_categories: str
+    created_date: datetime
+    updated_date: datetime
 
-    _id = db.Column("product_id", db.Integer, primary_key=True)
-    title = db.Column(db.String(30))
-    description = db.Column(db.String(150))
-    image_url = db.Column(db.String(1000))
-    link = db.Column(db.String(1000))
-    categories = db.relationship("Category", secondary=product_category)
-
+    product_id = db.Column("product_id", db.Integer, primary_key=True)
+    product_title = db.Column(db.String(50), nullable=False)
+    product_description = db.Column(db.String(150), nullable=False)
+    product_image_url = db.Column(db.String(1000), nullable=False)
+    product_link = db.Column(db.String(1000), nullable=False)
+    product_price = db.Column(db.Float, nullable=False)
+    product_gender = db.Column(db.Integer, nullable=False)  # Male=0, Female=1, Both=2
+    product_categories = db.relationship("Category", secondary=product_category)
+    created_date = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_date = db.Column(db.DateTime, onupdate=datetime.utcnow)
 
 @dataclass
 class Category(db.Model):
-    _id: int
-    name: str
-    #products: str
+    category_id: int
+    category_name: str
 
-    _id = db.Column("category_id", db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True)
-    #products = db.relationship("Product", secondary=product_category)
+    category_id = db.Column("category_id", db.Integer, primary_key=True)
+    category_name = db.Column(db.String(100), unique=True)
--- product_api.py
+++ product_api.py
@@ -1,16 +1,11 @@
 from flask import jsonify, request
 from app.db_models.models import Product, Category
+from sqlalchemy import exc
 from app import app
 from app import db
+from app.exceptions.SqlException import SqlException
 
-
-@app.route('/', methods=['GET'])
-def home():
-    return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
-
-
-# A route to return all of the available entries in our catalog.
+# Get all products
 @app.route('/api/v1/product/all', methods=['GET'])
 def api_all():
     return jsonify(Product.query.all())
@@ -18,20 +13,47 @@ def api_all():
 
 @app.route('/api/v1/product/create', methods=['POST'])
 def create_product():
-    category = Category(name="PostTest2ndCategory")
-    product = Product(title="Post Prod", description="Post Prod Desc", image_url="Post Image Url", link="Post link")
-    product.categories.append(category)
+    data = request.get_json()
+
+    categories = data['product_categories']
+    product = Product(product_title=data["product_title"],
+                      product_description=data["product_description"],
+                      product_image_url=data["product_image_url"],
+                      product_link=data["product_link"],
+                      product_price=data["product_price"],
+                      product_gender=data['product_gender'])
+
+    for _category in categories:
+        existing_cat = db.session.query(Category).filter_by(category_name=_category).first()
+        if existing_cat is None:
+            category = Category(category_name=_category)
+        else:
+            category = existing_cat
+        product.product_categories.append(category)
+
     db.session.add(product)
-    db.session.commit()
+    try:
+        db.session.commit()
+        db.session.refresh(product)
+        return str(product.product_id)
+    except exc.SQLAlchemyError:
+        db.session.rollback()
+        raise SqlException('Internal API Error', 500)
 
 
 @app.route('/api/v1/product/update', methods=['PUT'])
 def update_product():
-    postProduct = db.session.query(Product).filter_by(title="Post Prod").first()
-    category2 = Category(name="PostUpdateCategory")
-    postProduct.categories.append(category2)
+    post_product = db.session.query(Product).filter_by(product_id=request.args.get("product_id")).first()
+    category2 = Category(category_name="PostUpdateCategory")
+    post_product.categories.append(category2)
     db.session.commit()
 
+
+@app.errorhandler(SqlException)
+def handle_db_error(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 
 db.create_all()
 app.run()
