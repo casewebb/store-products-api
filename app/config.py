51f12130a90d9def54dfbeb20b1ed9c1077728aa
Case <34142234+casewebb@users.noreply.github.com>
2021-04-02 11:31:47 -0400

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 class Config(object):
-    SECRET_KEY = 'QVJuKEZa7we43Ctb'
+    SECRET_KEY = '1234'
     SQLALCHEMY_DATABASE_URI = 'mysql://root:admin@localhost/affiliate_store'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
